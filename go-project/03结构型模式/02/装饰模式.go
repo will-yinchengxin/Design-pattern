cbe4ef09afdd8a76a4a03c3c3fe3da3845f4b5f4
will yin <826895143@qq.com>
2022-11-17 23:15:17 +0800

    feat: add new summary

--- go-project/03结构型模式/02/装饰模式.go
+++ go-project/03结构型模式/02/装饰模式.go
@@ -1,5 +1,95 @@
 package main
 
-func main() {
+import "fmt"
 
+// ---------- 抽象层 ----------
+
+type Phone interface {
+	Show() //构件的功能
+}
+
+// Decorator 装饰器基础类（该类本应该为interface，但是Golang interface语法不可以有成员属性）
+type Decorator struct {
+	phone Phone
+}
+
+func (d *Decorator) Show() {}
+
+// ---------- 实现层 ----------
+// 具体手机
+
+type Huawei struct{}
+
+func (h *Huawei) Show() {
+	fmt.Println("\n啥也没有的 huawei")
+}
+
+type Xiaomi struct{}
+
+func (x *Xiaomi) Show() {
+	fmt.Println("\n啥也没有的 xiao mi")
+}
+
+// 具体装饰器
+type ShoujiKeDecorator struct {
+	Decorator
+}
+
+func (s *ShoujiKeDecorator) Show() {
+	s.phone.Show()
+	fmt.Println("安个壳")
+}
+
+func NewShoujiKeDecorator(phone Phone) Phone {
+	return &ShoujiKeDecorator{Decorator{phone}}
+}
+
+type ShoujiMoDecorator struct {
+	Decorator
+}
+
+func (s *ShoujiMoDecorator) Show() {
+	s.phone.Show()
+	fmt.Println("贴个膜")
+}
+
+func NewShoujiMoDecorator(phone Phone) Phone {
+	return &ShoujiMoDecorator{Decorator{phone}}
+}
+
+// ----------- 具体逻辑 -----------
+func main() {
+	// 裸机
+	huawei := &Huawei{}
+	huawei.Show()
+
+	// 带膜的
+	mohuawei := NewShoujiMoDecorator(huawei)
+	mohuawei.Show()
+
+	// 带壳的
+	kehuawei := NewShoujiKeDecorator(huawei)
+	kehuawei.Show()
+
+	// 带壳又带膜
+	kemoshouji := NewShoujiKeDecorator(mohuawei)
+	kemoshouji.Show()
+	/*
+		优点：
+			(1) 对于扩展一个对象的功能，装饰模式比继承更加灵活性，不会导致类的个数急剧增加。
+			(2) 可以通过一种动态的方式来扩展一个对象的功能，从而实现不同的行为。
+			(3) 可以对一个对象进行多次装饰。
+			(4) 具体构件类与具体装饰类可以独立变化，用户可以根据需要增加新的具体构件类和具体装饰类，原有类库代码无须改变，符合“开闭原则”。
+		缺点：
+			(1) 使用装饰模式进行系统设计时将产生很多小对象，大量小对象的产生势必会占用更多的系统资源，影响程序的性能。
+			(2) 装饰模式提供了一种比继承更加灵活机动的解决方案，但同时也意味着比继承更加易于出错，排错也很困难，对于多次装饰的对象，调试时寻找错误可能需要逐级排查，较为繁琐。
+
+		适用场景
+			(1) 动态、透明的方式给单个对象添加职责。
+			(2) 当不能采用继承的方式对系统进行扩展或者采用继承不利于系统扩展和维护时可以使用装饰模式。
+				装饰器模式关注于在一个对象上动态的添加方法，然而代理模式关注于控制对对象的访问。
+				换句话说，用代理模式，代理类（proxy class）可以对它的客户隐藏一个对象的具体信息。
+				因此，当使用代理模式的时候，我们常常在一个代理类中创建一个对象的实例。
+				并且，当我们使用装饰器模式的时候，我们通常的做法是将原始对象作为一个参数传给装饰者的构造器。
+	*/
 }
--- go-project/03结构型模式/03/适配器模式.go
+++ go-project/03结构型模式/03/适配器模式.go
@@ -1,5 +1,62 @@
 package main
 
-func main() {
+import "fmt"
 
+// 将一个类的接口转换成客户希望的另外一个接口。使得原本由于接口不兼容而不能一起工作的那些类可以一起工作。
+
+//适配的目标
+type V100 interface {
+	Use100V()
+}
+
+type JiayongDain struct {
+	v V100
+}
+
+func (j *JiayongDain) Use100V() {
+	j.v.Use100V()
+	fmt.Println("使用的是 100v 电")
+}
+
+//被适配的角色，适配者
+func NewJiayongDain(v100 V100) *JiayongDain {
+	return &JiayongDain{v100}
+}
+
+type GaoyaDian struct{}
+
+func (g *GaoyaDian) Use1000v() {
+	fmt.Println("1000v 高压电")
+}
+
+// 适配器
+type Adapter struct {
+	v1000 *GaoyaDian
+}
+
+func (a *Adapter) Use100V() {
+	a.v1000.Use1000v()
+	fmt.Println("开始适配")
+}
+
+func NewAdapter(v1000 *GaoyaDian) *Adapter {
+	return &Adapter{v1000: v1000}
+}
+
+// 业务裸机
+func main() {
+	jiayong := NewJiayongDain(NewAdapter(&GaoyaDian{}))
+	jiayong.Use100V()
+	/*
+		优点：
+			(1) 将目标类和适配者类解耦，通过引入一个适配器类来重用现有的适配者类，无须修改原有结构。
+			(2) 增加了类的透明性和复用性，将具体的业务实现过程封装在适配者类中，对于客户端类而言是透明的，而且提高了适配者的复用性，同一个适配者类可以在多个不同的系统中复用。
+			(3) 灵活性和扩展性都非常好，可以很方便地更换适配器，也可以在不修改原有代码的基础上增加新的适配器类，完全符合“开闭原则”。
+		缺点:
+			适配器中置换适配者类的某些方法比较麻烦。
+
+		适应场景
+			(1) 系统需要使用一些现有的类，而这些类的接口（如方法名）不符合系统的需要，甚至没有这些类的源代码。
+			(2) 想创建一个可以重复使用的类，用于与一些彼此之间没有太大关联的一些类，包括一些可能在将来引进的类一起工作。
+	*/
 }
